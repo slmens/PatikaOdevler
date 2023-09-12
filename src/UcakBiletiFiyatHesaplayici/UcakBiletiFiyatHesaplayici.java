35a503266d0895762b8608e3b4d0912ab61dfc36
Selim <selimenes527@outlook.com>
2023-09-12 22:53:25 +0300

    Bilet fiyatı hesaplayıcı programını değiştirdim

--- src/UcakBiletiFiyatHesaplayici/UcakBiletiFiyatHesaplayici.java
+++ src/UcakBiletiFiyatHesaplayici/UcakBiletiFiyatHesaplayici.java
@@ -24,6 +24,9 @@ public class UcakBiletiFiyatHesaplayici {
 
         total = mesafe * mesafeBasinaUcret;
 
+
+        // İlk kontrol
+
         if ((mesafe > 0) && (yas > 0) && ((yolculukTipi == 1) || (yolculukTipi == 2))){
 
 
@@ -49,6 +52,7 @@ public class UcakBiletiFiyatHesaplayici {
 
             System.out.printf("Bilet fiyatınız: %s TL'dir",indirimliTotal);
         }else {
+            //Hata durumunda buraya giriyor
             System.out.println("Hatalı bilgi girdiniz tekrar deneyin.");
             System.out.print("Fiyat hesaplayıcıya hoşgeldiniz. Lütfen gitmek istediğiniz mesafeyi km türünden giriniz: ");
             mesafe = input.nextInt();
@@ -60,6 +64,8 @@ public class UcakBiletiFiyatHesaplayici {
             yolculukTipi = input.nextInt();
 
             total = mesafe * mesafeBasinaUcret;
+
+            // İkinci Kontrol
 
             if ((mesafe > 0) && (yas > 0) && ((yolculukTipi == 1) || (yolculukTipi == 2))) {
 
@@ -86,6 +92,7 @@ public class UcakBiletiFiyatHesaplayici {
                 System.out.printf("Bilet fiyatınız: %s TL'dir", indirimliTotal);
 
             }else {
+                //Hata durumunda kapanıyor
                 System.out.println("Yine hatalı bilgi girdiniz. Güvenlik sebebiyle sistem kapatılıyor...");
                 System.exit(0);
             }
