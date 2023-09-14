4086658f16aff1f6f9a7bb97c5b1c51d860d92bb
Selim <selimenes527@outlook.com>
2023-09-14 11:59:58 +0300

    Ters üçgen yapan program

---
+++ src/TersUcgen/TersUcgen.java
@@ -0,0 +1,23 @@
+package TersUcgen;
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int basamakSayisi,counter = 0;
+
+        System.out.print("Yapmak istediğiniz yıldızın kaç basamaklı olacağını giriniz: ");
+        basamakSayisi = input.nextInt();
+
+        for (int t = basamakSayisi; t >= 0 ; t--) {
+            for (int j = 0; j < (basamakSayisi - t); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * t + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
