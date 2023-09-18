50a4b771fb34c5b7fa7aa23229d0e41d2ba454ab
slmens <selimenes527@outlook.com>
2023-09-18 12:45:18 +0300

    Maaş hesaplama programı yazdım

---
+++ src/Week2/MaasHesaplayici/Employee.java
@@ -0,0 +1,71 @@
+package Week2.MaasHesaplayici;
+
+public class Employee {
+    String name;
+    int salary;
+    int workHours;
+    int hireYear;
+    int totalSalary;
+    int taxAmount;
+    int bonusAmount;
+    int salaryRaise;
+
+    Employee(String name,int salary,int workHours,int hireYear){
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+        this.totalSalary = salary;
+
+        //Constructor metodundaki klasik değer verme işlerinin yanı sıra, metot yüklendiğinde diğer çalışması gereken metotları da otomatik olarak çalıştırsın diye
+        //Bu şekilde yazıyorum. Bu metotlar çalışıp, verilen değerlere göre hesaplamalara yapacaklar.
+        tax();
+        bonus();
+        raiseSalary();
+    }
+
+
+    //Eğer nesneye girilen maaş değeri 1000'in üstündeyse %3 kesinti yapıyoruz
+    public void tax(){
+        if (this.salary > 1000){
+            this.taxAmount = ((int) (salary * 0.03)) ;
+            totalSalary -= (int) (salary * 0.03);
+        }
+    }
+
+    //Eğer nesneye girilen çalışma saati 40'ın üstündeyse, çalıştığı her saat başına 30 TL bonus alıyor
+    public void bonus(){
+        if (this.workHours > 40){
+            this.bonusAmount = (workHours - 40) * 30;
+            totalSalary += bonusAmount;
+        }
+    }
+
+    //Bu metotda nesneye girilen işe girme yılını şuan ki yıldan çıkartarak, çalışanımızın kaç yıl çalıştığını buluyoruz ve bu sonuca göre maaşda artışlar yapıyoruz
+    public void raiseSalary(){
+        int workYear = 2021 - hireYear;
+        if (workYear > 19) {
+            salaryRaise = (int) (salary * 0.15);
+            totalSalary += salaryRaise;
+        }else if ((workYear) < 10){
+            salaryRaise = (int) (salary * 0.05);
+            totalSalary += salaryRaise;
+        }else if ((workYear > 9) && (workYear < 20)){
+            salaryRaise = (int) (salary * 0.1);
+            totalSalary += salaryRaise;
+        }
+    }
+
+    //Bu metot sadece değerleri ekrana yazdırmaya yarıyor
+    public void printMethod(){
+        System.out.println("Adı: " + this.name);
+        System.out.println("Eski maaşı: " + this.salary + " TL");
+        System.out.println("Çalışma saati: " + this.workHours);
+        System.out.println("Başlangıç yılı: " + this.hireYear);
+        System.out.println("Vergi: " + taxAmount);
+        System.out.println("Bonus: " + bonusAmount);
+        System.out.println("Maaş artışı: " + salaryRaise);
+        System.out.println("Vergi, bonus ve zamla beraber yeni maaş: " + totalSalary + " TL'dir.");
+    }
+
+}
---
+++ src/Week2/MaasHesaplayici/Main.java
@@ -0,0 +1,31 @@
+package Week2.MaasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Employee sınıfından employee nesnelerimizi yani çalışanlarımızı oluşturup onlara değer veriyoruz
+
+        Employee employee = new Employee("Ahmet",2000,65,2015);
+        Employee employee2 = new Employee("İlayda",2300,45,2000);
+        Employee employee3 = new Employee("İbrahim",1250,40,2020);
+        Employee employee4 = new Employee("Nazlı",1350,50,2018);
+        Employee employee5 = new Employee("Arda",1700,46,2009);
+        Employee employee6 = new Employee("Cenk",1800,38,2012);
+
+        //Employe sınıfındaki printMethod() metodunu çağırıyoruz. Bu metot nesnelerin değerlerini ekrana yazdırıyor
+
+        System.out.println("----------------------");
+        employee.printMethod();
+        System.out.println("----------------------");
+        employee2.printMethod();
+        System.out.println("----------------------");
+        employee3.printMethod();
+        System.out.println("----------------------");
+        employee4.printMethod();
+        System.out.println("----------------------");
+        employee5.printMethod();
+        System.out.println("----------------------");
+        employee6.printMethod();
+
+    }
+}
