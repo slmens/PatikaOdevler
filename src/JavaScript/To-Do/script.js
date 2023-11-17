ab82ddc992e67d59a5559829d3f1390585aaf345
slmens <selimenes527@outlook.com>
2023-11-17 20:52:23 +0300

    todolist not done

---
+++ src/JavaScript/To-Do/script.js
@@ -0,0 +1,153 @@
+const inputText = document.querySelector("#inputText");
+const inputButton = document.querySelector("#button-addon2");
+const todoList = document.querySelector("#main-ul");
+
+inputButton.addEventListener('click', addTodo);
+
+let todos = [];
+let arrayCount = localStorage.getItem("array-count") || 0;
+
+
+function addTodo() {
+    let pseudoCount = localStorage.getItem("array-count")
+    if (inputText.value === "") {
+        console.log("boş");
+    } else {
+        todos.push(inputText.value);
+        let todoString = JSON.stringify(todos);
+        localStorage.setItem("todo-array", todoString);
+
+        let listItem = document.createElement("li");
+        listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("class", "form-check-input me-1");
+        checkbox.setAttribute("value", "");
+        checkbox.setAttribute("id", `checkbox-${inputText.value}-${pseudoCount+1}`);
+
+        let label = document.createElement("label");
+        label.setAttribute("class", "form-check-label");
+        label.setAttribute("for", `checkbox-${inputText.value}-${pseudoCount+1}`);
+        label.textContent = inputText.value;
+
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("btn", "btn-danger", "btn-sm", "deletebtn");
+        deleteButton.setAttribute("id",`checkbox-${inputText.value}-${pseudoCount+1}`)
+
+        let checkboxLabelContainer = document.createElement("div");
+        checkboxLabelContainer.classList.add("d-flex", "align-items-center");
+        checkboxLabelContainer.appendChild(checkbox);
+        checkboxLabelContainer.appendChild(label);
+
+        listItem.appendChild(checkboxLabelContainer);
+        listItem.appendChild(deleteButton);
+
+        todoList.appendChild(listItem);
+
+        arrayCount++;
+        localStorage.setItem("array-count", arrayCount);
+
+        inputText.value = "";
+    }
+}
+
+function handleDelete(event) {
+    if (event.target.classList.contains("deletebtn")) {
+        console.log("Delete button clicked");
+        let deletedId = event.target.id
+        let existendCount = localStorage.getItem("array-count")
+        let finalCount = existendCount - 1
+        if(finalCount < 0){
+            finalCount = 0
+        }
+
+        let pseudoArr = localStorage.getItem("todo-array")
+        let parsedPseudoArr = JSON.parse(pseudoArr)
+
+        if (Array.isArray(parsedPseudoArr)) {
+            let finalPseudoArr = parsedPseudoArr.filter(item => `deletebtn-${item}-${parsedPseudoArr.indexOf(item)}` !== deletedId);
+
+            console.log(finalCount);
+            console.log(finalPseudoArr);
+
+            localStorage.setItem("todo-array", JSON.stringify(finalPseudoArr));
+            localStorage.setItem("array-count", finalCount);
+
+            loadPage();
+        } else {
+            console.log("Invalid data in localStorage");
+        }
+
+        let finalPseudoArrr = JSON.stringify(finalPseudoArr)
+
+        console.log(finalCount)
+        console.log(finalPseudoArrr)
+
+        localStorage.setItem("todo-array",finalPseudoArrr)
+        localStorage.setItem("array-count",finalCount)
+
+
+
+        loadPage()
+
+    }
+}
+
+// Use event delegation by attaching the event listener to the parent ul element
+todoList.addEventListener('click', handleDelete);
+
+
+function loadPage() {
+
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild);
+    }
+
+    if (todos[0] === "") {
+        console.log("array boş yüklenemiyor");
+    } else {
+        let localTodos = localStorage.getItem("todo-array");
+        let stringifiedArrayObj = JSON.parse(localTodos);
+        let localArrayCount = localStorage.getItem("array-count");
+
+        if (stringifiedArrayObj && stringifiedArrayObj.length > 0) {
+            for (let i = 0; i < stringifiedArrayObj.length; i++) {
+                let itemFromArray = stringifiedArrayObj[i];
+                let listItem = document.createElement("li");
+                listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+
+                let checkbox = document.createElement("input");
+                checkbox.setAttribute("type", "checkbox");
+                checkbox.setAttribute("class", "form-check-input me-1");
+                checkbox.setAttribute("value", "");
+                checkbox.setAttribute("id", `checkbox-${inputText.value}-${i}`);
+
+                let label = document.createElement("label");
+                label.setAttribute("class", "form-check-label");
+                label.setAttribute("for", `checkbox-${inputText.value}-${i}`);
+                label.textContent = itemFromArray;
+
+                let deleteButton = document.createElement("button");
+                deleteButton.textContent = "Delete";
+                deleteButton.classList.add("btn", "btn-danger", "btn-sm", "deletebtn");
+                deleteButton.setAttribute("id", `checkbox-${inputText.value}-${i}`);
+
+                let checkboxLabelContainer = document.createElement("div");
+                checkboxLabelContainer.classList.add("d-flex", "align-items-center");
+                checkboxLabelContainer.appendChild(checkbox);
+                checkboxLabelContainer.appendChild(label);
+
+                listItem.appendChild(checkboxLabelContainer);
+                listItem.appendChild(deleteButton);
+
+                todoList.appendChild(listItem);
+            }
+        } else {
+            console.log("Boş");
+        }
+    }
+}
+
+loadPage();
